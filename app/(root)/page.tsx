182c47a1506f7b56e80441b549d534ae940fd452
ldphat99 <ldphat99@gmail.com>
2023-11-25 23:31:50 +0700

    UPDATE: handle show msg when log success

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
 'use client'
 
+import { useSession } from "next-auth/react"
+import { message } from "antd";
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
-  return (
-    <main >
-      Thong ke 
-      
-    </main>
-  )
+    const { data: session, status } = useSession()
+
+    const [isSuccessMessageShown, setIsSuccessMessageShown] = useState(false);
+
+    useEffect(() => {
+        if (session && status === 'authenticated' && !isSuccessMessageShown) {
+            message.success('Login successful');
+            setIsSuccessMessageShown(true);
+        }
+    }, [session, status, isSuccessMessageShown]);
+
+
+
+    return (
+        <main >
+            Thong ke
+
+        </main>
+    )
 }
