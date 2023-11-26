70443b596e2612ecbdea446f9906d72c8c4e275e
ldphat99 <ldphat99@gmail.com>
2023-11-26 11:44:07 +0700

    UPDATE: handle delete product

--- app/(root)/product/page.tsx
+++ app/(root)/product/page.tsx
@@ -1,9 +1,10 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-import { Form, Input, InputNumber, Popconfirm, Table, Typography, Space, Tag } from 'antd';
+import React, { useState } from 'react';
+import { Table, Space, Tag, Button, Spin, message } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import useSWR from 'swr';
-import Image from 'next/image';
+import useSWR, { mutate, useSWRConfig } from 'swr';
+import { getSession } from 'next-auth/react';
+
 
 const fetcher = (url: any) => fetch(url).then(res => res.json())
 
@@ -18,78 +19,98 @@ interface DataType {
     tags: string[];
 }
 
-const columns: ColumnsType<DataType> = [
-    {
-        title: 'Id',
-        dataIndex: 'id',
-        key: 'id',
-    },
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: 'Image',
-        dataIndex: 'image',
-        key: 'image',
-    },
-    {
-        title: 'Price',
-        dataIndex: 'price',
-        key: 'price',
-    },
-    {
-        title: 'Quantity',
-        dataIndex: 'quantity',
-        key: 'quantity',
-    },
-    {
-        title: 'Category',
-        dataIndex: 'category',
-        key: 'category',
-    },
-    {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, { tags }) => (
-            <>
-                {tags.map((tag) => {
-                    const colors = ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple'];
 
-                    const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-                    return (
-                        <Tag color={randomColor} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </>
-        ),
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <a>Edit</a>
-                <a>Delete</a>
-            </Space>
-        ),
-    },
-];
 
 
 export default function Product() {
 
+    const columns: ColumnsType<DataType> = [
+        {
+            title: 'Id',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Image',
+            dataIndex: 'image',
+            key: 'image',
+        },
+        {
+            title: 'Price',
+            dataIndex: 'price',
+            key: 'price',
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'quantity',
+            key: 'quantity',
+        },
+        {
+            title: 'Category',
+            dataIndex: 'category',
+            key: 'category',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a>Edit</a>
+                    <button onClick={() => deleteProduct(record.id)}>Delete</button>
+                </Space>
+            ),
+        },
+    ];
+
+    const { data, error, isLoading, mutate } = useSWR(`${process.env.baseURI}/products?size=1000`, fetcher)
 
 
-    const { data, error, isLoading } = useSWR("http://localhost:8080/api/v1/products?size=1000", fetcher)
+    console.log("Data", data);
+    console.log("Error", error);
+    console.log("Isloading", isLoading);
 
-    console.log(data);
+
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    const deleteProduct = async (productId: string) => {
+        console.log('delete product with id =', productId);
+
+        const session = await getSession();
+
+        if (!session) {
+            return;
+        }
+
+        const accessToken = session.accessToken;
+
+        try {
+            setIsDeleting(true);
+            const response = await fetch(`${process.env.baseURI}/products/${productId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                }
+            });
+
+            if (response.ok) {
+                message.success(`Product with ID ${productId} deleted successfully.`);
+                mutate()
+            } else {
+                message.error(`Failed to delete product: ${response.statusText}`,);
+            }
+        } catch (error) {
+            message.error(`Something when wrong while deleting product: ${error}`);
+        } finally {
+            setIsDeleting(false);
+        }
+    }
 
 
     const dataSource: DataType[] = data?.content?.map((item: any, index: number) => ({
@@ -99,14 +120,16 @@ export default function Product() {
         image: <img src={`${item.media_url}`} alt='Card' sizes="100vw" width={500} height={300} className='h-14 rounded-sm w-full object-cover' />,
         price: item.price,
         quantity: item.quantity,
-        category: item.category,
-        tags: item.tag.split(','),
+        category: <Tag color={item.category === 'flower' ? 'gold' : 'blue'}>{item.category}</Tag>,
+        // tags: item.tag.split(','),
     }))
 
     return (
-        <>
-            {/* <img src={`https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg`} alt='Card' sizes="100vw" width={500} height={300} className='h-44 rounded-2xl w-full object-cover' /> */}
-            <Table columns={columns} dataSource={dataSource} />
-        </>
+        <div>
+            <Spin spinning={isLoading || isDeleting}>
+                <Button type="primary" className='float-right mb-4' >Add</Button>
+                <Table columns={columns} dataSource={dataSource} bordered />
+            </Spin>
+        </div>
     )
 }
