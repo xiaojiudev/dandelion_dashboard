446ab88d106bbb5055e2b29d298a93a7bf0fa7e2
ldphat99 <ldphat99@gmail.com>
2023-11-26 11:44:31 +0700

    UPDATE: add baseuri in config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['i.ebayimg.com', 'images.pexels.com','randomuser.me', 'pixahive.com', 'res.cloudinary.com'],
-
     },
+    env: {
+        baseURI: process.env.BACKEND_URI
+    }
 }
 
 module.exports = nextConfig
